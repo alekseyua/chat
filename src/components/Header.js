b89d2f308a0782f1852a7567f8d0a1275bbf694f
Alekseyuadnepr <alekseyuadnepr@gmail.com>
2022-07-29 13:41:04 +0300

    new test push git

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+                <img src='https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/HTML/Element/img/favicon144.png' alt='icon profile'/>
+        </header>
+    )
+}
+
+export default Header;
