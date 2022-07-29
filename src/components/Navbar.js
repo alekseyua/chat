1fa27e51da5bc566ccfa930b9e4791e60c229c06
Alekseyuadnepr <alekseyuadnepr@gmail.com>
2022-07-29 08:08:23 +0300

    start project chat

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav className='nav'>
+        <div>
+          <a  href='#'>profile</a>
+        </div>
+        <div>
+        <a  href='#'>message</a>
+        </div>
+        <div>
+        <a href='#'>news</a>
+        </div>
+        <div>
+        <a href='#'>music</a>
+        </div>
+        <div>
+        <a href='#'>settings</a>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className='content'>
+        <div className='contant-img'>
+          <img src='https://th.bing.com/th/id/R.c68dda9ba3e0f857241a7d45f2ebf211?rik=YslxNS1Q5FuEeg&pid=ImgRaw&r=0' alt='workboard pic'/>
+        </div>
+        <div className='post-avatar'>
+          <img src='https://www.seoclerk.com/pics/000/748/061/bd1ddcf20243b5366e14524b6b79c773.png' alt='avatar user'/>
+        </div>
+        <div>ava + discription</div>
+        <div>
+          My post
+          <div>
+            New Post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
